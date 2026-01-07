661eaeb8621766e5e6ab5802bdcac061288946a7
Eman Hjazi <emanhjazi12@gmail.com>
2026-01-07 08:56:12 +0200

    feat(profile): add mobile field with validation

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -25,6 +25,12 @@ class ProfileUpdateRequest extends FormRequest
                 'max:255',
                 Rule::unique(User::class)->ignore($this->user()->id),
             ],
+            'mobile' => [
+                'nullable',
+                'string',
+                'max:16',
+                Rule::unique(User::class)->ignore($this->user()->id),
+            ],
         ];
     }
 }
