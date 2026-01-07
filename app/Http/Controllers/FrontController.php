56ff32007b501ac04f3b18ef927eac91e64ece12
Eman Hjazi <emanhjazi12@gmail.com>
2026-01-07 15:03:32 +0200

    feat(front): render dynamic data on frontend pages

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Skill;
+use App\Models\Language;
+use App\Models\Education;
+use App\Models\Experience;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
@@ -10,25 +15,25 @@ class FrontController extends Controller
     public function index()
     {
         return view('front.index');
-
     }
 
     public function resume()
     {
-        return view('front.resume');
-
+        $experiences = Experience::latest()->get();
+        $educations = Education::latest()->get();
+        $skills = Skill::latest()->get();
+        $languages = Language::latest()->get();
+        return view('front.resume', compact('experiences', 'educations', 'skills', 'languages'));
     }
 
     public function projects()
     {
-        return view('front.projects');
+        $projects = Project::latest()->get();
+        return view('front.projects', compact('projects'));
     }
 
     public function contact()
     {
         return view('front.contact');
     }
-
-
-
 }
